aebe9ed44151ec7b5ac6e04f2cf72e08c95f2208
0xDmitry <sign-69@mail.ru>
2024-08-14 16:43:16 +0400

    Remove express

--- packages/telegram-bot/api/index.ts
+++ packages/telegram-bot/api/index.ts
@@ -1,6 +1,5 @@
 import { Telegraf } from "telegraf"
 import { message } from "telegraf/filters"
-// import express from "express"
 
 import dotenv from "dotenv"
 dotenv.config()
@@ -9,7 +8,7 @@ const bot = new Telegraf(process.env.TG_BOT_TOKEN!)
 
 bot.start((ctx) =>
   ctx.reply(
-    "Welcome to the TON based wargame Hack the TON! Wow you are so early!"
+    "Welcome to the TON based wargame Hack the TON! Wow, you are so early!"
   )
 )
 
@@ -26,13 +25,3 @@ bot.launch({
 // Enable graceful stop
 process.once("SIGINT", () => bot.stop("SIGINT"))
 process.once("SIGTERM", () => bot.stop("SIGTERM"))
-
-// const app = express()
-
-// app.use(await bot.createWebhook({ domain: process.env.WEBHOOK_DOMAIN! }))
-
-// app.get("/", (_, res) => {
-//   res.send("Running")
-// })
-
-// export default app
