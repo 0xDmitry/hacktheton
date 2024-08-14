1cbc2549b6830c61b473d8318a91fd7487113f90
0xDmitry <sign-69@mail.ru>
2024-08-14 13:33:33 +0400

    Setup telegram bot

---
+++ packages/telegram-bot/api/index.ts
@@ -0,0 +1,23 @@
+import { Telegraf } from "telegraf"
+import { message } from "telegraf/filters"
+import express from "express"
+
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+
+const bot = new Telegraf(process.env.TG_BOT_TOKEN!)
+app.use(await bot.createWebhook({ domain: process.env.WEBHOOK_DOMAIN! }))
+
+bot.start((ctx) => ctx.reply("Hello!"))
+
+bot.on(message("text"), async (ctx) => {
+  await ctx.reply("Yeah yeah yeah")
+})
+
+bot.launch()
+
+app.get("/", (_, res) => {
+  res.send("Telegram bot is ready!")
+})
