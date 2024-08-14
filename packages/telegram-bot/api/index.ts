f80b7930ac4545150fe0cd63fd08b7faff64943b
0xDmitry <sign-69@mail.ru>
2024-08-14 14:01:28 +0400

    Add bot welcome message

--- packages/telegram-bot/api/index.ts
+++ packages/telegram-bot/api/index.ts
@@ -13,13 +13,15 @@ app.use(await bot.createWebhook({ domain: process.env.WEBHOOK_DOMAIN! }))
 bot.start((ctx) => ctx.reply("Hello!"))
 
 bot.on(message("text"), async (ctx) => {
-  await ctx.reply("Yeah yeah yeah")
+  await ctx.reply(
+    "Welcome to the TON based wargame Hack the TON! You are so early..."
+  )
 })
 
 bot.launch()
 
 app.get("/", (_, res) => {
-  res.send("Telegram bot is ready!")
+  res.send("Coming soon!")
 })
 
 export default app
