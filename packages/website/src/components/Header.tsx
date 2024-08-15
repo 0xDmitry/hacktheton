c2ef24a19e5c53c60764c5d96997bc74b795d7e6
0xDmitry <sign-69@mail.ru>
2024-08-15 16:53:21 +0400

    Minor UI improvements

--- packages/website/src/components/Header.tsx
+++ packages/website/src/components/Header.tsx
@@ -4,11 +4,11 @@ import { HackTheTon } from "@/components/assets/HackTheTon"
 export const Header = () => (
   <header className="container mx-auto flex items-center justify-center sm:justify-between p-6">
     <Link href="/">
-      <div className="flex items-center gap-6 cursor-pointer">
+      <div className="flex items-center gap-6 select-none">
         <HackTheTon />
-        <div className="text-4xl select-none">HACK THE TON</div>
+        <div className="text-4xl">HACK THE TON</div>
       </div>
     </Link>
-    <div>Connect Wallet</div>
+    {/* <div>Connect Wallet</div> */}
   </header>
 )
