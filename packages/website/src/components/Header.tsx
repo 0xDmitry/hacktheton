b7b7a02b82bed78ac64854496758b303a304874c
0xDmitry <sign-69@mail.ru>
2024-08-17 01:17:19 +0400

    Fix header

--- packages/website/src/components/Header.tsx
+++ packages/website/src/components/Header.tsx
@@ -3,11 +3,11 @@ import { HackTheTon } from "@/components/assets/HackTheTon"
 import { ConnectButton } from "@/components/ConnectButton"
 
 export const Header = () => (
-  <header className="container mx-auto flex items-center justify-center sm:justify-between p-6">
+  <header className="container mx-auto flex items-center justify-between p-6">
     <Link href="/">
       <div className="flex items-center gap-6 select-none">
         <HackTheTon />
-        <div className="text-4xl">HACK THE TON</div>
+        <div className="text-4xl hidden sm:block">HACK THE TON</div>
       </div>
     </Link>
     <ConnectButton />
