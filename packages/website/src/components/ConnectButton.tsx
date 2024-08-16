4b1d8ece8ff8fd605712b4ba3d35384f83218850
0xDmitry <sign-69@mail.ru>
2024-08-17 01:05:08 +0400

    Connect button styling

--- packages/website/src/components/ConnectButton.tsx
+++ packages/website/src/components/ConnectButton.tsx
@@ -2,4 +2,6 @@
 
 import { TonConnectButton } from "@tonconnect/ui-react"
 
-export const ConnectButton = () => <TonConnectButton />
+export const ConnectButton = () => (
+  <TonConnectButton className="[&_button]:bg-foreground [&_button_*]:text-black [&_button_*]:fill-black" />
+)
