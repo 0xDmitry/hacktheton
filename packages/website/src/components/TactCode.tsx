42a9cdd9ea6037605569e5486fe34389778e9a3d
KseniaFilippova <filippovaki@yandex.ru>
2024-08-18 20:29:33 +0300

    TactCode component is server side rendered

--- packages/website/src/components/TactCode.tsx
+++ packages/website/src/components/TactCode.tsx
@@ -1,21 +1,10 @@
-"use client"
-
-import { useEffect, useState } from "react"
 import { highlightTactCode } from "@/utils/highlight-tact-code"
 
-export const TactCode = ({ code }: { code: string }) => {
-  const [tactCodeHtml, setTactCodeHtml] = useState("")
-
-  // TODO: Can ve load on server side?
-  useEffect(() => {
-    async function wait() {
-      setTactCodeHtml(await highlightTactCode(code))
-    }
-    wait()
-  }, [code])
+export const TactCode = async ({ code }: { code: string }) => {
+  const tactCodeHtml = await highlightTactCode(code)
 
   return (
-    <div className="panelCode w-10/12">
+    <div className="panelCode w-full">
       <div className="h-full overflow-scroll">
         <div
           className="dark"
