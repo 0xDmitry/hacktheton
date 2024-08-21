bf96abe4de1ef809d9b220d05c43206d271736cb
KseniaFilippova <filippovaki@yandex.ru>
2024-08-21 12:36:20 +0400

    Add 404 default page

---
+++ packages/website/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="container mx-auto flex flex-col justify-center text-center gap-2 px-6 py-72">
+      <h2 className="text-3xl">Oh no!</h2>
+      <p className="text-xl">Looks like the page you are looking is missing</p>
+    </div>
+  )
+}
