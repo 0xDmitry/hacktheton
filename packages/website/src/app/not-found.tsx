9a99f07ed6e8b24614e3fa135c2900d6f8d4f77c
KseniaFilippova <filippovaki@yandex.ru>
2024-08-21 12:48:24 +0400

    Fix not found page

--- packages/website/src/app/not-found.tsx
+++ packages/website/src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className="container mx-auto flex flex-col justify-center text-center gap-2 px-6 py-72">
+    <div className="container mx-auto flex flex-col justify-center text-center gap-2 p-6">
       <h2 className="text-3xl">Oh no!</h2>
       <p className="text-xl">Looks like the page you are looking is missing</p>
     </div>
