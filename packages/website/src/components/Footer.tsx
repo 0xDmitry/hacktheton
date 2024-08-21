6c4e65abc6a407935e4f74ee41530d5785e3a387
KseniaFilippova <filippovaki@yandex.ru>
2024-08-21 18:52:48 +0400

    Social media links click events are tracked

--- packages/website/src/components/Footer.tsx
+++ packages/website/src/components/Footer.tsx
@@ -5,22 +5,38 @@ import { GitHub } from "@/components/assets/socials/GitHub"
 
 export const Footer = () => (
   <footer className="flex justify-center items-center p-6 gap-2">
-    <a href="https://x.com/hacktheton" target="_blank">
+    <a
+      href="https://x.com/hacktheton"
+      target="_blank"
+      data-umami-event="Twitter link click"
+    >
       <div className="rounded-full size-full hover:bg-gray-600 transition duration-500">
         <Twitter />
       </div>
     </a>
-    <a href="https://t.me/hacktheton" target="_blank">
+    <a
+      href="https://t.me/hacktheton"
+      target="_blank"
+      data-umami-event="Telegram link click"
+    >
       <div className="rounded-full size-full hover:bg-gray-600 transition duration-500">
         <Telegram />
       </div>
     </a>
-    <a href="https://discord.gg/vaFRDfG4uz" target="_blank">
+    <a
+      href="https://discord.gg/vaFRDfG4uz"
+      target="_blank"
+      data-umami-event="Discord link click"
+    >
       <div className="rounded-full size-full hover:bg-gray-600 transition duration-500">
         <Discord />
       </div>
     </a>
-    <a href="https://github.com/0xDmitry/hacktheton" target="_blank">
+    <a
+      href="https://github.com/0xDmitry/hacktheton"
+      target="_blank"
+      data-umami-event="Github link click"
+    >
       <div className="rounded-full size-full hover:bg-gray-600 transition duration-500">
         <GitHub />
       </div>
