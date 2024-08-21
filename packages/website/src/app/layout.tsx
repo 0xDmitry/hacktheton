c55bfd91b699056d65ca33cc487538e04d7355d8
0xDmitry <sign-69@mail.ru>
2024-08-21 17:33:59 +0400

    Add umami

--- packages/website/src/app/layout.tsx
+++ packages/website/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next"
 import { Anonymous_Pro } from "next/font/google"
+import Script from "next/script"
 import "./globals.css"
 import { Header } from "@/components/Header"
 import { Footer } from "@/components/Footer"
@@ -25,6 +26,11 @@ export default function RootLayout({
         <div className="grow flex bg-background">{children}</div>
         <Footer />
       </body>
+      <Script
+        defer
+        src="https://cloud.umami.is/script.js"
+        data-website-id="8563ccf8-341c-4bea-a79b-b542d63644a8"
+      />
     </html>
   )
 }
