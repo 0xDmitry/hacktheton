38dcb7b2f6db1c5e1370f767f9c1e18529c51b87
KseniaFilippova <filippovaki@yandex.ru>
2024-08-26 15:16:42 +0400

    Add locale switcher component

--- packages/website/src/components/LocaleSwitcher.tsx
+++ packages/website/src/components/LocaleSwitcher.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-import { setCookie } from "cookies-next"
-import { usePathname } from "next/navigation"
+import { useState, useEffect, useRef } from "react"
+import { setCookie, getCookie } from "cookies-next"
+import { useParams, usePathname } from "next/navigation"
 import Link from "next/link"
 import { i18n, Locale } from "@/i18n.config"
+import { ArrowDown } from "@/components/assets/ArrowDown"
 
 export const LocaleSwitcher = () => {
   const pathName = usePathname()
+  const params = useParams()
 
   const redirectedPathName = (locale: Locale) => {
     if (!pathName) return "/"
@@ -15,23 +18,62 @@ export const LocaleSwitcher = () => {
     return segments.join("/")
   }
 
-  const setLocaleCookie = (locale: Locale) => {
-    setCookie("lang", locale)
+  const [open, setOpen] = useState(false)
+
+  const [currentValue, setCurrentValue] = useState<Locale>(
+    (params.lang as Locale) || i18n.defaultLocale
+  )
+
+  const dropdownRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const closeDropdown = (e: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target as Node)
+      ) {
+        setOpen(false)
+      }
+    }
+    document.body.addEventListener("click", closeDropdown)
+    return () => document.body.removeEventListener("click", closeDropdown)
+  }, [])
+
+  const handleClick = (locale: Locale) => {
+    if (getCookie("lang") !== locale) {
+      setCookie("lang", locale)
+    }
+
+    setCurrentValue(locale)
+    setOpen(false)
   }
 
   return (
-    <ul className="flex gap-x-3">
-      {i18n.locales.map((locale) => (
-        <li key={locale}>
-          <Link
-            href={redirectedPathName(locale)}
-            onClick={() => setLocaleCookie(locale)}
-            className="rounded-md border border-foreground bg-background px-3 py-2"
-          >
-            {locale}
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <div className="relative">
+      <button
+        onClick={() => setOpen(true)}
+        className="flex justify-center items-center gap-1 w-14 h-8 border border-foreground rounded-md bg-background hover:bg-gray-600 transition"
+      >
+        {currentValue}
+        <ArrowDown />
+      </button>
+      {open ? (
+        <div
+          ref={dropdownRef}
+          className="flex flex-col w-14 absolute top-10 rounded-md border border-foreground divide-y divide-foreground transition"
+        >
+          {i18n.locales.map((locale) => (
+            <Link
+              key={locale}
+              href={redirectedPathName(locale)}
+              onClick={() => handleClick(locale)}
+              className="flex items-center justify-center h-8 bg-background hover:bg-gray-600 first:rounded-t-md last:rounded-b-md"
+            >
+              {locale}
+            </Link>
+          ))}
+        </div>
+      ) : null}
+    </div>
   )
 }
