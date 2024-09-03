0462fec59061d6ee56f6e79945153d9f3e09338e
KseniaFilippova <filippovaki@yandex.ru>
2024-09-03 16:28:51 +0400

    Fix locale bug

--- packages/website/src/components/Header.tsx
+++ packages/website/src/components/Header.tsx
@@ -1,16 +1,22 @@
+"use client"
+
 import Link from "next/link"
+import { useParams } from "next/navigation"
 import { HackTheTon } from "@/components/assets/HackTheTon"
 import { LocaleSwitcher } from "@/components/LocaleSwitcher"
 
-export const Header = () => (
-  <header className="container mx-auto flex items-center justify-between p-6">
-    <Link href="/">
-      <div className="flex items-center gap-6 select-none">
-        <HackTheTon />
-        <div className="text-4xl hidden sm:block">HACK THE TON</div>
-      </div>
-    </Link>
-    <LocaleSwitcher />
-    {/* <div>Connect Wallet</div> */}
-  </header>
-)
+export const Header = () => {
+  const params = useParams()
+  return (
+    <header className="container mx-auto flex items-center justify-between p-6">
+      <Link href={`/${params.locale}`}>
+        <div className="flex items-center gap-6 select-none">
+          <HackTheTon />
+          <div className="text-4xl hidden sm:block">HACK THE TON</div>
+        </div>
+      </Link>
+      <LocaleSwitcher />
+      {/* <div>Connect Wallet</div> */}
+    </header>
+  )
+}
