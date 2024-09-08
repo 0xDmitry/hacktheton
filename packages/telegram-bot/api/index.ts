29f05ea3c45af0c135d5d4eacb8b6d5b38447027
0xDmitry <sign-69@mail.ru>
2024-09-08 19:15:34 +0400

    Fix bot

--- packages/telegram-bot/api/index.ts
+++ packages/telegram-bot/api/index.ts
@@ -1,20 +1,11 @@
 import { Telegraf } from "telegraf"
-import { message } from "telegraf/filters"
 
 import dotenv from "dotenv"
 dotenv.config()
 
 const bot = new Telegraf(process.env.TG_BOT_TOKEN!)
 
-bot.start((ctx) =>
-  ctx.reply(
-    "Welcome to the TON based wargame Hack the TON! Wow, you are so early!"
-  )
-)
-
-bot.on(message("text"), (ctx) => {
-  ctx.reply("Coming soon!")
-})
+bot.start((ctx) => ctx.reply("Welcome to the TON based wargame Hack the TON!"))
 
 bot.launch({
   webhook: {
