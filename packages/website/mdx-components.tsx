7a0bceb3620ea17eb94b21c55bc997ca6cd8430f
KseniaFilippova <filippovaki@yandex.ru>
2025-01-17 17:12:59 +0400

    Update intro level text (russian)

--- packages/website/mdx-components.tsx
+++ packages/website/mdx-components.tsx
@@ -2,20 +2,22 @@ import type { MDXComponents } from "mdx/types"
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    h1: ({ children }) => <h1 className="text-lg pt-7 pb-2">{children}</h1>,
-    h2: ({ children }) => <h2 className="text-base pt-5 pb-2">{children}</h2>,
-    p: ({ children }) => <p className="text-base p-1">{children}</p>,
+    h1: ({ children }) => <h1 className="text-lg py-2">{children}</h1>,
+    h2: ({ children }) => <h2 className="text-base py-2">{children}</h2>,
+    p: ({ children }) => <p className="text-sm">{children}</p>,
+    ol: ({ children }) => (
+      <ol className="list-decimal py-2 pb-4">{children}</ol>
+    ),
+    li: ({ children }) => <li className="pb-2">{children}</li>,
     code: ({ children }) => (
-      <code className="font-anonymousPro text-base text-slate-400">
-        {children}
-      </code>
+      <code className="font-anonymousPro text-slate-400">{children}</code>
     ),
     a: (props) => {
       return (
         <a
           href={props.href}
           target="_blank"
-          className="text-base text-slate-400 hover:text-slate-300"
+          className="text-slate-400 hover:text-slate-300"
         >
           {props.children}
         </a>
