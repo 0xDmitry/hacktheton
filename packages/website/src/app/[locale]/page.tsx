773781e2a4b9d4525c81d862777319921f19a921
KseniaFilippova <filippovaki@yandex.ru>
2025-02-06 13:33:30 +0400

    Leaderboard link on main page

--- packages/website/src/app/[locale]/page.tsx
+++ packages/website/src/app/[locale]/page.tsx
@@ -1,5 +1,6 @@
+import Link from "next/link"
+import { ArrowForward } from "@/components/assets/ArrowForward"
 import { LevelsList } from "@/components/LevelsList"
-import { TypewriterText } from "@/components/TypewriterText"
 import { Welcome } from "@/components/Welcome"
 import { Locale } from "@/i18n.config"
 import { getLangDictionary } from "@/utils/lang-dictionary"
@@ -16,6 +17,15 @@ export default function Home({
   return (
     <div className="container mx-auto flex flex-col justify-center gap-12 my-6 p-6">
       <Welcome locale={locale} />
+      <div className="flex justify-center items-center">
+        <Link
+          className="flex justify-center items-center gap-1 text-2xl tracking-wide text-slate-400 hover:text-slate-300"
+          href={`/${locale}/leaderboard`}
+        >
+          {home.leaderboard}
+          <ArrowForward />
+        </Link>
+      </div>
       <LevelsList />
     </div>
   )
---
+++ packages/website/src/components/assets/ArrowForward.tsx
@@ -0,0 +1,14 @@
+export const ArrowForward = () => (
+  <svg
+    stroke="currentColor"
+    fill="currentColor"
+    strokeWidth="0"
+    viewBox="0 0 24 24"
+    height="20px"
+    width="20px"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path fill="none" d="M0 0h24v24H0V0z"></path>
+    <path d="M6.23 20.23 8 22l10-10L8 2 6.23 3.77 14.46 12z"></path>
+  </svg>
+)
